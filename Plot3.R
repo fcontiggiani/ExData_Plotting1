7a7a5b6e9ae9cd22c8a1928d0c97ab37423ebcca
Federico <federico.contiggiani@gmail.com>
2015-06-07 12:30:00 -0300

    Change in the code and the plot3

--- Plot3.R
+++ Plot3.R
@@ -23,7 +23,7 @@ with(data, {
     lines(Sub_metering_2~Datetime,col='Red')
     lines(Sub_metering_3~Datetime,col='Blue')
 })
-legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, 
+legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2.5, 
        legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3")) ## Plot 3
 
 
