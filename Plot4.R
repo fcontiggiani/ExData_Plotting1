dff733bcf2bd86fe1093765313e89ecabc6fa072
Federico <federico.contiggiani@gmail.com>
2015-06-07 15:32:43 -0300

    Course Project 1. Final Version
    Project 1 for the course Exploratory Data Analysis at Coursera.

--- Plot4.R
+++ Plot4.R
@@ -22,7 +22,7 @@ with(data, {
     plot(Global_active_power~Datetime, type="l", 
          ylab="Global Active Power (kilowatts)", xlab="")
     plot(Voltage~Datetime, type="l", 
-         ylab="Voltage (volt)", xlab="")
+         ylab="Voltage (volt)", xlab="datetime")
     plot(Sub_metering_1~Datetime, type="l", 
          ylab="Energy sub meterin", xlab="")
     lines(Sub_metering_2~Datetime,col='Red')
@@ -30,7 +30,7 @@ with(data, {
     legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n",
            legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
     plot(Global_reactive_power~Datetime, type="l", 
-         ylab="Global Rective Power (kilowatts)",xlab="")
+         ylab="Global Rective Power (kilowatts)",xlab="datetime")
 }) ## Plot 4.
 
 dev.copy(png, file="plot4.png", height=480, width=480)
